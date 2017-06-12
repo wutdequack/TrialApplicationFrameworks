dc3c21b23a951aa42326b4d4d537ecacfbd00fb0
wutdequack <jonathanleejuler@gmail.com>
2017-06-12 14:31:35 +0800

    hello stanley

--- app/src/main/java/com/app/ostiarius/trialapplicationframeworks/AlertsActivity.java
+++ app/src/main/java/com/app/ostiarius/trialapplicationframeworks/AlertsActivity.java
@@ -13,5 +13,6 @@ public class AlertsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.alert_layout);ssss
+        // hello stanley
     }
 }
